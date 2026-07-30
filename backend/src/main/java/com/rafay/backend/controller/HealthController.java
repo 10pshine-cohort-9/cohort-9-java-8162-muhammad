56835e8ecb585a35d2ac781ec2978b1ff2ff69c4
Muhammad Rafay Imran <imranrafay01@gmail.com>
2026-07-30 13:51:09 +0500

    Initial Spring Boot project setup

---
+++ backend/src/main/java/com/rafay/backend/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.rafay.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Contact Management System API is running!";
+    }
+}
